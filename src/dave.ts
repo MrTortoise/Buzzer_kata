b2c2c126d1efe4cd8413437cb57f980f0d4dd7d9
John Nicholas <john.nicholas@digital.cabinet-office.gov.uk>
2022-10-06 21:34:05 +0100

    dave has a job

--- src/dave.ts
+++ src/dave.ts
@@ -5,13 +5,27 @@ import { TemperatureMeasured } from "./propietarySourceCode/sensor";
 
 /*
 This is the aggregate of the system.
+What is an aggreagte?
+A system has many objects usually - the aggregate is the one that people interact with.
+It might be a search system, it might be their shopping basket.
+Both these things have very different owners and functions.
+
 This system may only have one part so one may ask aggregate of what.
 Dave had a job, now he does not.
 Daves only job was to take temperatures and apply force to the appropiate button
 */
 export class Dave implements IApply<TemperatureMeasured>{
   async apply(event: TemperatureMeasured): Promise<void> {
-    console.log(event.temperature)
+    const buzzer = new Buzzer()
+    if (event.temperature >= 80) {
+      buzzer.start()
+    }
   }
+}
 
+
+export class Buzzer {
+  start(): void {
+    console.log("BUZZ BUZZ ZZZZ No Bees Harmed Buzzz")
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { App } from "./propietarySourceCode/app";
 import { Dave } from "./dave";
 
-const app = new App(new Dave());
+let app = new App(new Dave());
 
 (async () => {
   await app.start()
   console.log("started")
   await app.waitTillStopped()
+  app = null
   console.log("stopped")
 })()
 
